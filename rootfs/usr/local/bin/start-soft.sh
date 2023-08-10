ebeae110fad204f886089576e8ab0f8f4add574c
casjay <git-admin@casjaysdev.com>
2023-08-10 19:57:06 -0400

    🗃️ Updated domain names 🗃️
    Dockerfile
    LICENSE.md
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/setup_soft-serve.sh
    rootfs/usr/local/bin/start-soft.sh

--- rootfs/usr/local/bin/start-soft.sh
+++ rootfs/usr/local/bin/start-soft.sh
@@ -3,7 +3,7 @@
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ##@Version           :  202210251748-git
 # @@Author           :  Jason Hempstead
-# @@Contact          :  jason@casjaysdev.com
+# @@Contact          :  jason@casjaysdev.pro
 # @@License          :  WTFPL
 # @@ReadME           :  start-soft.sh --help
 # @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
